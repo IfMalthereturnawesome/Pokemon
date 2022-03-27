879d9eb15068d9616e3b82f066ed7d99e34b0fb4
IfMalthereturnawesome <malt996b@stud.kea.dk>
2022-03-27 18:33:02 +0200

    Changed Default sortField to ID

--- src/main/java/com/example/pokemonthymeleafentity/controller/HomeController.java
+++ src/main/java/com/example/pokemonthymeleafentity/controller/HomeController.java
@@ -29,7 +29,7 @@ public class HomeController {
     @RequestMapping("/")
     public String index(Model model){
 
-        return viewPage(model,"name", "asc");
+        return viewPage(model,"id", "asc");
     }
 
 
